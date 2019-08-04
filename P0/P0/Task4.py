354b42f0f1677314c63783d323001e52d375336b
Bato <dengbotaoo@gmail.com>
2019-08-03 22:54:46 -0400

    finish task4

--- P0/P0/Task4.py
+++ P0/P0/Task4.py
@@ -25,3 +25,33 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+def find_unique_nums(numbers, col):
+    res = set()
+    for row in numbers:
+        number = row[col]
+        res.add(number)
+    return res
+
+def find_telemarketers(texts, calls):
+    text_senders = find_unique_nums(texts, 0)
+    text_receivers = find_unique_nums(texts, 1)
+    call_senders = find_unique_nums(calls, 0)
+    call_receivers = find_unique_nums(calls, 1)
+
+    candidates =  call_senders-call_receivers-text_senders-text_receivers
+
+    return sorted(candidates)
+
+def run():
+    result = find_telemarketers(texts, calls)
+    print(f"These numbers could be telemarketers:{result}")
+
+run()
+
+
+"""
+Assume texts has m rows, and calls has n rows.
+
+Time Complexity: O(n^2+m^2)
+Space Complexity: O(n+m)
+"""
