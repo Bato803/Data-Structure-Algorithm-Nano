ee540ff47534120c7a76f7f9ff7d21463fb73967
Bato <dengbotaoo@gmail.com>
2019-09-03 14:50:08 -0400

    added diameter of tree/deletion of BST node

--- Tree/BST_delete.py
+++ Tree/BST_delete.py
@@ -12,21 +12,35 @@ class Solution(object):
         :type key: int
         :rtype: TreeNode
         """
-        if root.val==key:
-            newVal = self.findNxtLargest(root.right)
-            root.val = newVal
+        if not root:
             return
 
-        if root.val>key:
-            self.deleteNode(root.left, key)
-        elif root.val<key:
-            self.deleteNode(root.right, key)
+        if key>root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key<root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left and not root.right:
+                root = None
+            elif root.left:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
 
         return root
 
-    def findNxtLargest(self, root):
-        if not root.left:
-            val = root.val
-            root.val = None
-            return val
-        self.findNxtLargest(self, root.left)
+    def predecessor(self, root):
+        root = root.left
+        while root.right:
+            root = root.right
+
+        return root.val
+
+    def successor(self, root):
+        root = root.right
+        while root.left:
+            root = root.left
+
+        return root.val
